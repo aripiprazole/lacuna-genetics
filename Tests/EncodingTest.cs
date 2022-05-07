2a6820cbcda234f93ba0195f2dc7bdc946b3ac17
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2022-05-07 18:15:06 -0300

    Add job tasks tests

---
+++ Tests/EncodingTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using LacunaGenetics.Jobs;
+using NUnit.Framework;
+
+namespace Tests;
+
+public class EncodingTest
+{
+    [Test]
+    public void TestEncoding()
+    {
+        var strandEncoded = Job.EncodeStrand("CATCGTCAGGAC");
+
+        Assert.AreEqual("TbSh", strandEncoded);
+    }
+
+    [Test]
+    public void TestDecoding()
+    {
+        var strand = Job.DecodeStrand("TbSh");
+
+        Assert.AreEqual("CATCGTCAGGAC", strand);
+    }
+}
---
+++ Tests/GeneTest.cs
@@ -0,0 +1,25 @@
+﻿using LacunaGenetics.Jobs;
+using NUnit.Framework;
+
+namespace Tests;
+
+public class GeneTest
+{
+    [Test]
+    public void TestNotContains50Percent()
+    {
+        Assert.IsFalse(Job.CheckGene("string", "ABCDEFJHIJKL"));
+    }
+    
+    [Test]
+    public void TestContains50Percent()
+    {
+        Assert.IsTrue(Job.CheckGene("ABCDEFJHI", "ABCDEFJHIJKL"));
+    }
+    
+    [Test]
+    public void TestContains100Percent()
+    {
+        Assert.IsTrue(Job.CheckGene("ABCDEFJHIJKL", "ABCDEFJHIJKL"));
+    }
+}
